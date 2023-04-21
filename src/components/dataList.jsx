1d10a6b1b2dda857134b855c146a418764e2404d
Melkijo <jonathanandaraa@gmail.com>
2023-04-21 13:23:36 +0800

    fix: data image

--- src/components/dataList.jsx
+++ src/components/dataList.jsx
@@ -1,30 +1,36 @@
 import port1 from "../assets/img/port1.svg";
+import port2 from "../assets/img/port2.svg";
+import port3 from "../assets/img/port3.svg";
+import port4 from "../assets/img/port4.svg";
+import user1 from "../assets/img/user-1.png";
+import user2 from "../assets/img/user-2.png";
+import user3 from "../assets/img/user-3.png";
 
 export const portfolios = [
-  { id: 1, image: { port1 } },
-  { id: 2, image: "./src/assets/img/port2.svg" },
-  { id: 3, image: "./src/assets/img/port3.svg" },
-  { id: 4, image: "./src/assets/img/port4.svg" },
+  { id: 1, image: port1 },
+  { id: 2, image: port2 },
+  { id: 3, image: port3 },
+  { id: 4, image: port4 },
 ];
 
 export const testimonials = [
   {
     id: 1,
-    image: "./src/assets/img/user-1.png",
+    image: user1,
     name: "Agil Wibawa",
     comment:
       "Saya sangat senang dengan hasil website yang dibuat oleh individu ini. Cepat, mudah, dan sesuai dengan keinginan saya. Terima kasih banyak",
   },
   {
     id: 2,
-    image: "./src/assets/img/user-2.png",
+    image: user2,
     name: "Habib el",
     comment:
       "Individu ini sangat berpengalaman dalam membuat website. Saya sangat terkesan dengan kualitas kerjanya dan akan merekomendasikan jasa pembuatan website ini kepada teman-teman saya.",
   },
   {
     id: 3,
-    image: "./src/assets/img/user-3.png",
+    image: user3,
     name: "Evalius",
     comment:
       "Saya sangat puas dengan layanan jasa pembuatan website yang diberikan oleh individu ini. Selain hasilnya yang memuaskan, harganya juga sangat terjangkau. Terima kasih banyak dan sukses selalu!",
