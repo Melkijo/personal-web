2a4bc2c4fad3b734d2a08c55174ae069ff44c7e6
Melkijo <jonathanandaraa@gmail.com>
2023-06-28 16:48:50 +0800

    up

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,7 +1,7 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import personalImg from "../assets/img/persona1.svg";
+import personalImg from "/assets/img/persona1.png";
 export default function AboutUs() {
    return (
       <>
