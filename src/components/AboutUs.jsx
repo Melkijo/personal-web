751bf8ab94093c6f82c9d9819a72903e741ed72e
Melkijo <jonathanandaraa@gmail.com>
2023-06-28 16:52:58 +0800

    fix: about img

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,7 +1,7 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import personalImg from "/assets/img/persona1.png";
+import personalImg from "../assets/img/persona.png";
 export default function AboutUs() {
    return (
       <>
